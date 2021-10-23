ad2a1c47b870c066d186d496bb0ee1a3f3d0e71b
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2021-10-23 17:33:26 -0300

    Faltan algunos procedures

--- scripts/script_creacion_inicial.sql
+++ scripts/script_creacion_inicial.sql
@@ -19,8 +19,8 @@ GO
 
 CREATE TABLE [brog].[Materiales] (
   [mate_id] nvarchar(100),
-  [mate_descripcion] nvarchar(255) NULL,
-  [mate_precio] decimal(18,2) NULL,
+  [mate_descripcion] nvarchar(255) NOT NULL,
+  [mate_precio] decimal(18,2) NOT NULL,
   CONSTRAINT PK_Materiales PRIMARY KEY ([mate_id])
 )
 
@@ -30,8 +30,8 @@ IF OBJECT_ID ('brog.Tarea', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[Tarea] (
   [tare_id] int identity(1,1),
-  [tarea_tipo] nvarchar(255) NULL,
-  [tarea_tiempo_estimado] int NULL,
+  [tarea_tipo] nvarchar(255) NOT NULL,
+  [tarea_tiempo_estimado] int NOT NULL,
   CONSTRAINT PK_Tarea PRIMARY KEY ([tare_id])
 )
 
@@ -42,11 +42,11 @@ IF OBJECT_ID ('brog.Taller', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[Taller] (
   [tall_id] int identity(1,1),
-  [tall_direccion] nvarchar(255) NULL,
-  [tall_telefono] decimal(18,0) NULL,
-  [tall_mail] nvarchar(255) NULL,
-  [tall_nombre] nvarchar(255) NULL,
-  [tall_ciudad] nvarchar(255) NULL,
+  [tall_direccion] nvarchar(255) NOT NULL,
+  [tall_telefono] decimal(18,0) NOT NULL,
+  [tall_mail] nvarchar(255) NOT NULL,
+  [tall_nombre] nvarchar(255) NOT NULL,
+  [tall_ciudad] nvarchar(255) NOT NULL,
   CONSTRAINT PK_Taller PRIMARY KEY ([tall_id])
 )
 
@@ -55,10 +55,10 @@ IF OBJECT_ID ('brog.Recorrido', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[Recorrido] (
   [reco_id] int identity(1,1),
-  [reco_ciudad_dest] nvarchar(255) NULL,
-  [reco_ciudad_origen] nvarchar(255) NULL,
-  [reco_precio] decimal(18,2) NULL,
-  [reco_km] int NULL,
+  [reco_ciudad_dest] nvarchar(255) NOT NULL,
+  [reco_ciudad_origen] nvarchar(255) NOT NULL,
+  [reco_precio] decimal(18,2) NOT NULL,
+  [reco_km] int NOT NULL,
   CONSTRAINT PK_Recorrido PRIMARY KEY ([reco_id])
 )
 
@@ -68,12 +68,12 @@ IF OBJECT_ID ('brog.Tipo_paquete', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[Tipo_paquete] (
   [tipa_id] int identity(1,1),
-  [tipa_descripcion] nvarchar(255) NULL,
-  [tipa_peso_max] decimal(18,2) NULL,
-  [tipa_alto_max] decimal(18,2) NULL,
-  [tipa_ancho_max] decimal(18,2) NULL,
-  [tipa_largo_max] decimal(18,2) NULL,
-  [tipa_precio] decimal(18,2) NULL,
+  [tipa_descripcion] nvarchar(255) NOT NULL,
+  [tipa_peso_max] decimal(18,2) NOT NULL,
+  [tipa_alto_max] decimal(18,2) NOT NULL,
+  [tipa_ancho_max] decimal(18,2) NOT NULL,
+  [tipa_largo_max] decimal(18,2) NOT NULL,
+  [tipa_precio] decimal(18,2) NOT NULL,
   CONSTRAINT PK_Tipo_paquete PRIMARY KEY ([tipa_id])
 )
 
@@ -82,9 +82,9 @@ IF OBJECT_ID ('brog.PaquetexViaje', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[PaquetexViaje] (
   [paqx_id] int,
-  [paqx_cantidad] Int,
-  [paqx_viaje] int,
-  [paqx_paquete] int,
+  [paqx_cantidad] Int NOT NULL,
+  [paqx_viaje] int NOT NULL,
+  [paqx_paquete] int NOT NULL,
   CONSTRAINT PK_PaquetexViaje PRIMARY KEY ([paqx_id])
 );
 
@@ -94,17 +94,16 @@ IF OBJECT_ID ('brog.Chofer', 'U') IS NOT NULL
    DROP TABLE brog.Chofer; 
 GO
 CREATE TABLE [brog].[Chofer] (
-  [chof_id] int identity(1,1),
-  [chof_nombre] nvarchar(255) NULL,
-  [chof_apellido] nvarchar(255) NULL,
-  [chof_direccion] nvarchar(255) NULL,
-  [chof_mail] nvarchar(255) NULL,
-  [chof_legajo] int NULL,
-  [chof_dni] decimal(18,0) NULL,
-  [chof_telefono] int NULL,
-  [chof_fecha_nac] datetime2(3) NULL,
-  [chof_costo_hora] int NULL,
-  CONSTRAINT PK_Chofer PRIMARY KEY ([chof_id])
+  [chof_legajo] int,
+  [chof_nombre] nvarchar(255) NOT NULL,
+  [chof_apellido] nvarchar(255) NOT NULL,
+  [chof_direccion] nvarchar(255) NOT NULL,
+  [chof_mail] nvarchar(255) NOT NULL,
+  [chof_dni] decimal(18,0) NOT NULL,
+  [chof_telefono] int NOT NULL,
+  [chof_fecha_nac] datetime2(3) NOT NULL,
+  [chof_costo_hora] int NOT NULL,
+  CONSTRAINT PK_Chofer PRIMARY KEY ([chof_legajo])
 )
 
 IF OBJECT_ID ('brog.Modelo', 'U') IS NOT NULL  
@@ -112,11 +111,11 @@ IF OBJECT_ID ('brog.Modelo', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[Modelo] (
   [mode_id] int identity(1,1),
-  [mode_velocidad_max] int NULL,
-  [mode_capacidad_tanque] int NULL,
-  [mode_capacidad_carga] int NULL,
-  [mode_nombre] nvarchar(255) NULL,
-  [mode_marca] nvarchar(255) NULL,
+  [mode_nombre] nvarchar(255) NOT NULL,
+  [mode_marca] nvarchar(255) NOT NULL,
+  [mode_velocidad_max] int NOT NULL,
+  [mode_capacidad_tanque] int NOT NULL,
+  [mode_capacidad_carga] int NOT NULL,
   CONSTRAINT PK_Modelo PRIMARY KEY ([mode_id])
 )
 
@@ -125,11 +124,11 @@ IF OBJECT_ID ('brog.Camion', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[Camion] (
   [cami_id] int identity(1,1),
-  [cami_patente] nvarchar(255) NULL,
-  [cami_nro_chasis] nvarchar(255) NULL,
-  [cami_nro_motor] nvarchar(255) NULL,
-  [cami_fecha_alta] datetime2(3) NULL,
-  [cami_modelo] int,
+  [cami_patente] nvarchar(255) NOT NULL,
+  [cami_nro_chasis] nvarchar(255) NOT NULL,
+  [cami_nro_motor] nvarchar(255) NOT NULL,
+  [cami_fecha_alta] datetime2(3) NOT NULL,
+  [cami_modelo] int NOT NULL,
   CONSTRAINT PK_Camion PRIMARY KEY ([cami_id])
 )
 
@@ -138,8 +137,8 @@ IF OBJECT_ID ('brog.Orden_trabajo', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[Orden_trabajo] (
   [ot_id] int identity(1,1),
-  [ot_camion] int,
-  [ot_fecha_realizacion] nvarchar(255) NULL,
+  [ot_camion] int NOT NULL,
+  [ot_fecha_realizacion] nvarchar(255) NOT NULL,
   CONSTRAINT PK_Orden_trabajo PRIMARY KEY ([ot_id])
 )
 
@@ -147,25 +146,25 @@ IF OBJECT_ID ('brog.MaterialesXtarea', 'U') IS NOT NULL
    DROP TABLE brog.MaterialesXtarea; 
 GO
 CREATE TABLE [brog].[MaterialesXtarea] (
-  [mxt_material] nvarchar(100),
-  [mxt_tarea] int NULL,
-  [mxt_cantidad] int NULL
+  [mxt_material] nvarchar(100) NOT NULL,
+  [mxt_tarea] int NOT NULL,
+  [mxt_cantidad] int NOT NULL
 )
 
 IF OBJECT_ID ('brog.Mecanico', 'U') IS NOT NULL  
    DROP TABLE brog.Mecanico; 
 GO
 CREATE TABLE [brog].[Mecanico] (
-  [meca_legajo] int identity(1,1),
-  [meca_nombre] nvarchar(255) NULL,
-  [meca_apellido] nvarchar(255) NULL,
-  [meca_dni] decimal(18,0) NULL,
-  [meca_direccion] nvarchar(255) NULL,
-  [meca_telefono] int NULL,
-  [meca_mail] nvarchar(255) NULL,
-  [meca_fechaNac] datetime2(3) NULL,
-  [meca_costoHora] int NULL,
-  [meca_taller] int NULL,
+  [meca_legajo] int ,
+  [meca_nombre] nvarchar(255) NOT NULL,
+  [meca_apellido] nvarchar(255) NOT NULL,
+  [meca_dni] decimal(18,0) NOT NULL,
+  [meca_direccion] nvarchar(255) NOT NULL,
+  [meca_telefono] int NOT NULL,
+  [meca_mail] nvarchar(255) NOT NULL,
+  [meca_fechaNac] datetime2(3) NOT NULL,
+  [meca_costoHora] int NOT NULL,
+  [meca_taller] int NOT NULL,
   CONSTRAINT PK_Mecanico PRIMARY KEY ([meca_legajo])
 )
 
@@ -173,14 +172,14 @@ IF OBJECT_ID ('brog.OtXtarea', 'U') IS NOT NULL
    DROP TABLE brog.OtXtarea; 
 GO
 CREATE TABLE [brog].[OtXtarea] (
-  [otxt_orden_trabajo] int,
-  [otxt_tarea] int,
-  [otxt_mecanico] int,
-  [otxt_estado_tarea] nvarchar(255) NULL,
-  [otxt_fecha_inicio_estimada] datetime2(3) NULL,
-  [otxt_fecha_inicio] datetime2(3) NULL,
-  [otxt_fecha_fin] datetime2(3) NULL,
-  [otxt_tiempo_real] int NULL
+  [otxt_orden_trabajo] int NOT NULL,
+  [otxt_tarea] int NOT NULL,
+  [otxt_mecanico] int NOT NULL,
+  [otxt_estado_tarea] nvarchar(255) NOT NULL,
+  [otxt_fecha_inicio_estimada] datetime2(3) NOT NULL,
+  [otxt_fecha_inicio] datetime2(3) NOT NULL,
+  [otxt_fecha_fin] datetime2(3) NOT NULL,
+  [otxt_tiempo_real] int NOT NULL
 )
 
 IF OBJECT_ID ('brog.Viaje', 'U') IS NOT NULL  
@@ -188,12 +187,12 @@ IF OBJECT_ID ('brog.Viaje', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[Viaje] (
   [viaj_id] int identity(1,1),
-  [viaj_camion] int,
-  [viaj_chof] int,
-  [viaj_recorrido] int,
-  [viaj_fecha_inicio] datetime2(7) NULL,
-  [viaj_fecha_fin] datetime2(3) NULL,
-  [viaj_consumo_combustible] decimal(18,2) NULL,
+  [viaj_camion] int NOT NULL,
+  [viaj_chof] int NOT NULL,
+  [viaj_recorrido] int NOT NULL,
+  [viaj_fecha_inicio] datetime2(7) NOT NULL,
+  [viaj_fecha_fin] datetime2(3) NOT NULL,
+  [viaj_consumo_combustible] decimal(18,2) NOT NULL,
   CONSTRAINT PK_Viaje PRIMARY KEY ([viaj_id])
 )
 
@@ -202,11 +201,231 @@ IF OBJECT_ID ('brog.Paquete', 'U') IS NOT NULL
 GO
 CREATE TABLE [brog].[Paquete] (
   [paqu_id] int identity(1,1),
-  [paqu_tipo] int,
+  [paqu_tipo] int NOT NULL,
   CONSTRAINT PK_Paquete PRIMARY KEY ([paqu_id])
 )
 
+-----------------------------------------
+-- PROCEDURES PARA MIGRACION
 
+
+-- MIGRACION MATERIALES
+
+IF OBJECT_ID ('brog.migracionMateriales', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionMateriales; 
+GO
+create procedure brog.migracionMateriales
+as
+begin
+	insert into brog.Materiales
+	select distinct MATERIAL_COD,MATERIAL_DESCRIPCION,MATERIAL_PRECIO from gd_esquema.Maestra where MATERIAL_COD <> 'null'
+end
+go
+
+-- MIGRACION TAREAS
+	
+IF OBJECT_ID ('brog.migracionTareas', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionTareas; 
+GO
+
+create procedure brog.migracionTareas
+as
+begin
+	insert into brog.Tarea
+	select distinct TIPO_TAREA,TAREA_TIEMPO_ESTIMADO from gd_esquema.Maestra where tipo_tarea <> 'null'
+
+end
+
+
+-- MIGRACION TALLER
+
+IF OBJECT_ID ('brog.migracionTaller', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionTaller; 
+GO
+
+create procedure brog.migracionTaller
+as
+begin
+	insert into brog.Taller
+	select distinct TALLER_DIRECCION,TALLER_TELEFONO,TALLER_MAIL,  TALLER_NOMBRE,TALLER_CIUDAD from gd_esquema.Maestra where TALLER_DIRECCION <> 'null'
+
+end
+
+
+-- MIGRACION RECORRIDO
+
+IF OBJECT_ID ('brog.migracionRecorrido', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionRecorrido; 
+GO
+
+create procedure brog.migracionRecorrido
+as
+begin
+	insert into brog.Recorrido
+	select distinct RECORRIDO_CIUDAD_DESTINO,RECORRIDO_CIUDAD_ORIGEN,RECORRIDO_PRECIO,RECORRIDO_KM from gd_esquema.Maestra where RECORRIDO_CIUDAD_DESTINO <> 'null'
+	
+end
+
+
+-- MIGRACION TIPO PAQUETE
+
+IF OBJECT_ID ('brog.migracionTipoPaquete', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionTipoPaquete; 
+GO
+
+create procedure brog.migracionTipoPaquete
+as
+begin
+	insert into brog.Tipo_paquete
+	select PAQUETE_DESCRIPCION, PAQUETE_PESO_MAX, PAQUETE_ALTO_MAX, PAQUETE_ANCHO_MAX, PAQUETE_LARGO_MAX, PAQUETE_PRECIO from gd_esquema.Maestra where PAQUETE_DESCRIPCION <> 'null'
+
+end
+
+
+-- MIGRACION CHOFER
+
+IF OBJECT_ID ('brog.migracionChofer', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionChofer; 
+GO
+
+create procedure brog.migracionChofer
+as
+begin
+	insert into brog.Chofer
+	select distinct CHOFER_NOMBRE, CHOFER_APELLIDO, CHOFER_DIRECCION,CHOFER_MAIL,CHOFER_NRO_LEGAJO,CHOFER_DNI,CHOFER_TELEFONO,CHOFER_FECHA_NAC,CHOFER_COSTO_HORA from gd_esquema.Maestra where CHOFER_NOMBRE <> 'null'
+end
+
+
+-- MIGRACION MODELO
+
+IF OBJECT_ID ('brog.migracionModelo', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionModelo; 
+GO
+
+create procedure brog.migracionModelo
+as
+begin
+	insert into brog.Modelo
+	select distinct MODELO_CAMION, MARCA_CAMION_MARCA, MODELO_VELOCIDAD_MAX,MODELO_CAPACIDAD_TANQUE,MODELO_CAPACIDAD_CARGA from gd_esquema.Maestra where CHOFER_NOMBRE <> 'null'
+	
+end
+
+-- MIGRACION CAMION
+
+IF OBJECT_ID ('brog.migracionCamion', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionCamion; 
+GO
+
+create procedure brog.migracionCamion
+as
+begin
+	insert into brog.Camion
+	select distinct CAMION_PATENTE, CAMION_NRO_CHASIS, CAMION_NRO_MOTOR,CAMION_FECHA_ALTA, mode_id
+	from gd_esquema.Maestra join Modelo on mode_nombre = MODELO_CAMION
+	where CAMION_PATENTE <> 'null'
+end
+
+-- MIGRACION DE ORDEN DE TRABAJO
+
+IF OBJECT_ID ('brog.migracionOT', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionOT; 
+GO
+
+create procedure brog.migracionOT
+as
+begin
+	insert into brog.Orden_trabajo
+	select DISTINCT ORDEN_TRABAJO_FECHA, cami_id
+	from gd_esquema.Maestra join Camion on cami_patente = CAMION_PATENTE
+	where ORDEN_TRABAJO_FECHA <> 'NULL'
+	
+end
+
+-- MIGRACION MATERIAL POR TAREA
+
+IF OBJECT_ID ('brog.migracionMaterialxTarea', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionMaterialxTarea; 
+GO
+
+create procedure brog.migracionMaterialxTarea
+as
+begin
+	insert into brog.MaterialesXtarea
+	--select MATERIAL from gd_esquema.Maestra where CHOFER_NOMBRE <> 'null'
+	
+end
+
+-- MIGRACION MECANICO
+
+IF OBJECT_ID ('brog.migracionMecanico', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionMecanico; 
+GO
+
+create procedure brog.migracionMecanico
+as
+begin
+	insert into brog.Mecanico
+	select distinct MECANICO_NRO_LEGAJO,MECANICO_APELLIDO,MECANICO_DNI,MECANICO_DIRECCION,MECANICO_TELEFONO,MECANICO_MAIL,MECANICO_FECHA_NAC,MECANICO_COSTO_HORA, tall_id
+	from gd_esquema.Maestra join Taller on TALLER_NOMBRE = tall_nombre
+	where CHOFER_NOMBRE <> 'null'
+	
+end
+
+-- MIGRACION ORDEN X TAREA
+
+IF OBJECT_ID ('brog.migracionModelo', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionModelo; 
+GO
+
+create procedure brog.migracionModelo
+as
+begin
+	insert into brog.Modelo
+	select distinct MODELO_CAMION,MODELO_VELOCIDAD_MAX,MODELO_CAPACIDAD_TANQUE,MODELO_CAPACIDAD_CARGA,MARCA_CAMION_MARCA from gd_esquema.Maestra where CHOFER_NOMBRE <> 'null'
+	
+end
+
+-- MIGRACION VIAJE
+
+IF OBJECT_ID ('brog.migracionModelo', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionModelo; 
+GO
+
+create procedure brog.migracionModelo
+as
+begin
+	insert into brog.Modelo
+	select distinct MODELO_CAMION,MODELO_VELOCIDAD_MAX,MODELO_CAPACIDAD_TANQUE,MODELO_CAPACIDAD_CARGA,MARCA_CAMION_MARCA from gd_esquema.Maestra where CHOFER_NOMBRE <> 'null'
+	
+end
+
+-- MIGRACION PAQUETE
+
+IF OBJECT_ID ('brog.migracionModelo', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionModelo; 
+GO
+
+create procedure brog.migracionModelo
+as
+begin
+	insert into brog.Modelo
+	select distinct MODELO_CAMION,MODELO_VELOCIDAD_MAX,MODELO_CAPACIDAD_TANQUE,MODELO_CAPACIDAD_CARGA,MARCA_CAMION_MARCA from gd_esquema.Maestra where CHOFER_NOMBRE <> 'null'
+	
+end
+
+-- MIGRACION PAQUETE X VIAJE
+
+IF OBJECT_ID ('brog.migracionModelo', 'P') IS NOT NULL  
+   DROP PROCEDURE brog.migracionModelo; 
+GO
+
+create procedure brog.migracionModelo
+as
+begin
+	insert into brog.Modelo
+	select distinct MODELO_CAMION,MODELO_VELOCIDAD_MAX,MODELO_CAPACIDAD_TANQUE,MODELO_CAPACIDAD_CARGA,MARCA_CAMION_MARCA from gd_esquema.Maestra where CHOFER_NOMBRE <> 'null'
+	
+end
 
 
 IF OBJECT_ID('migracion','P') IS NOT NULL
